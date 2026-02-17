f63abf35fcd6bb26d79b6e92cf484ec0b43052c3
Amarah Morgan <amarahmorgan@gmail.com>
2026-02-17 14:41:44 +0200

    setting up environment, ts file structure

--- Practice_Programmes/boolean/card_status.ts
+++ Practice_Programmes/boolean/card_status.ts
@@ -1,7 +1,7 @@
 // card status 
 let isCardActive: boolean = true ;
 
-if (isCardActive) {
+if (isCardActive === true) {
     console.log('Card is active');
 } else{
     console.log('Card is blocked');
